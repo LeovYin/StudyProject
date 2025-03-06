86e099a63497acdc57da571876d548913c3bc5bb
LoevYin <gjy776868312@163.com>
2025-03-06 22:39:44 +0800

    login

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from service.login_service import *
 
 app = Flask(__name__)
@@ -19,6 +19,19 @@ def check_credentials(username, password):
 @app.route('/login')
 def indexLogin():
     return render_template('login.html', failure=False)
+
+
+# 处理登录请求
+@app.route('/login', methods=['post'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+    if check_credentials(username, password):
+        # 登录成功，重定向到首页或其他页面
+        return redirect(url_for('indexPart2'))
+    else:
+        # 登录失败，显示错误消息
+        return render_template('login.html', failure=True)
 
 
 if __name__ == '__main__':
