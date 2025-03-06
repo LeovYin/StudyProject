3caf2f742a80e22538b93482b80ab8c195c2cb15
LoevYin <gjy776868312@163.com>
2025-03-06 22:28:56 +0800

    login

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 from flask import Flask, render_template
+from service.login_service import *
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
+def index():  # put application's code here
+    return render_template('login.html', failure=False)
+
+
+# 检查用户凭证
+def check_credentials(username, password):
+    user = get_user(username, password)
+    return user is not None
+
+
+# 登录页面
+@app.route('/login')
+def indexLogin():
     return render_template('login.html', failure=False)
 
 
---
+++ service/login_service.py
@@ -0,0 +1,32 @@
+import pymysql
+
+# 建立数据库连接
+conn = pymysql.connect(host='localhost', port=3306, user='root', password="776868312",
+                       database='studyproject', charset="utf8mb4", cursorclass=pymysql.cursors.DictCursor)
+
+def get_user(username, password):
+    sql = "SELECT * FROM user WHERE username = %s AND password = %s"
+    try:
+        conn.ping(reconnect=True)
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        cursor.execute(sql, (username, password))
+        result = cursor.fetchone()
+        return result
+    except pymysql.MySQLError as e:
+        return type(e), e
+    finally:
+        cursor.close()
+
+def get_user_by_username(username):
+    sql = "SELECT * FROM user WHERE username = %s"
+    try:
+        conn.ping(reconnect=True)
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        cursor.execute(sql, (username,))
+        result = cursor.fetchone()
+        return result
+    except pymysql.MySQLError as e:
+        return type(e), e
+    finally:
+        cursor.close()
+
